cdbda10f292001ae073b37cc5703cc0e16d63be5
Chayapat Chadilalai <chayapatc@github.com>
2016-03-29 15:19:16 +0700

    Allow CORS

--- app.js
+++ app.js
@@ -22,6 +22,11 @@ mongoose.connect(`mongodb://${config.database.host}/${config.database.name}`);
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // models
 var Product = require('./models/Product.js')(mongoose);
